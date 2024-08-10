1211def141c95aa6a824c42fe033431952173c6e
koishibuh <elysiagriffin@gmail.com>
2024-08-10 20:33:17 +0100

    SCOPES

---
+++ Koishibot.Core/Common/StreamerScopes.cs
@@ -0,0 +1,38 @@
+﻿namespace Koishibot.Core.Common;
+
+public static class ScopesStreamer
+{
+	public static List<string> Scopes = new List<string> {
+		"bits:read",
+		"channel:moderate",
+		"channel:read:ads",
+		"channel:read:charity",
+		"channel:read:hype_train",
+		"channel:read:goals",
+		"channel:read:subscriptions",
+		"channel:manage:broadcast",
+		"channel:manage:polls",
+		"channel:manage:predictions",
+		"channel:manage:redemptions",
+		"channel:manage:vips",
+		"moderation:read",
+		"moderator:read:followers",
+		"moderator:read:moderators",
+		"moderator:read:suspicious_users",
+		"moderator:read:vips",
+		"moderator:manage:announcements",
+		"moderator:manage:automod",
+		"moderator:manage:unban_requests",
+		"moderator:manage:banned_users",
+		"moderator:manage:blocked_terms",
+		"moderator:manage:chat_messages",
+		"moderator:manage:chat_settings",
+		"moderator:manage:shield_mode",
+		"moderator:manage:shoutouts",
+		"moderator:manage:unban_requests",
+		"moderator:manage:warnings",
+		"user:read:chat",
+		"user:manage:whispers",
+		"user:write:chat"
+		};
+}
