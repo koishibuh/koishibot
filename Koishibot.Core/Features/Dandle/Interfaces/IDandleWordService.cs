4ad6c075b5964fde14aa14104a2f79f8e9373662
koishibuh <elysiagriffin@gmail.com>
2024-10-15 19:20:18 +0100

    Added FindWord command in Dandle

--- Koishibot.Core/Features/Dandle/Interfaces/IDandleWordService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Koishibot.Core.Features.ChatMessages.Models;
-namespace Koishibot.Core.Features.Dandle.Interfaces;
-
-public interface IDandleWordService
-{
-	Task CreateWord(ChatMessageDto c);
-	Task DeleteWord(ChatMessageDto c);
-	Task DefineWord(string word);
-}
