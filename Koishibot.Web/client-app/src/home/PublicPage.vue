c71c506fb64e25fb9784f70fc010223b3286041d
koishibuh <elysiagriffin@gmail.com>
2024-12-06 21:55:38 +0000

    - Added temp public landing page - Added temp style to login

---
+++ Koishibot.Web/client-app/src/home/PublicPage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col w-full h-dvh bg-secondary">
+    <div class="hidden fixed lg:block left-0 w-full fill-primary origin-center rotate-180 top-0 overflow-hidden z-10">
+      <svg class="relative w-119 h-[250px]" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+      </svg>
+    </div>
+
+    <div class="hidden fixed left-0 w-full lg:block fill-primary bottom-0 overflow-hidden z-10">
+      <svg class="relative w-119 h-[250px]" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+      </svg>
+    </div>
+
+    <div class="p-5 border-2 border-black rounded w-fit m-auto flex flex-col gap-2 text-center bg-background text-foreground">
+      <p>Ohhi, nothing to see here..yet.</p>
+        <a class="underline hover:text-accent-one" href="https://www.twitch.tv/elysiagriffin">Twitch Stream</a>
+    </div>
+  </div>
+</template>
