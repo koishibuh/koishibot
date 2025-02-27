7b0b69e4a7034644f207c36e6d4c1938f4c293b6
koishibuh <elysiagriffin@gmail.com>
2025-02-27 18:14:34 +0000

    - Added "Dev Mode" on menu to avoid confusion between dev and live versions of the bot

--- Koishibot.Web/client-app/src/layout/NavBar.vue
+++ Koishibot.Web/client-app/src/layout/NavBar.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import {computed} from "vue";
+
+const isDevMode = computed(() => import.meta.env.MODE === 'development');
 </script>
 
 <template>
+  <p v-if="isDevMode" class="p-2 bg-red-400 text-center">🛠️ DEV MODE</p>
   <nav class="flex flex-col justify-between mt-[1px]">
     <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
     <RouterLink :to="{ name: 'Attendance' }">Attendance</RouterLink>
